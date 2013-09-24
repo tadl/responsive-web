7ed65ba31c7a0ca78e793eeb57e440a014901b77
William Rockwood <wrockwood@tadl.org>
2013-09-24 16:46:55 -0400

    online and youth pages, minor tweaks and cleanup
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- js/main-page-functions.js
+++ js/main-page-functions.js
@@ -21,18 +21,18 @@ var FEED_VIDEO_MET = 'https://www.tadl.org/mobile/export/items/286/all/json'; //
 var FEED_VIDEO_REVIEWS = 'https://www.tadl.org/export/reviews/Video/json'; //middle
 
 /* Feeds for Online page */
-var FEED_ONLINE_MEL = ''; //middle-middle
-var FEED_ONLINE_RESOURCES = ''; //top-middle
-var FEED_ONLINE_LEGAL = ''; //middle-middle
-var FEED_ONLINE_EBOOKS = ''; //???
+var FEED_ONLINE_MEL = 'https://www.tadl.org/export/node/json/3373'; //middle-middle
+var FEED_ONLINE_RESOURCES = 'https://www.tadl.org/export/node/json/3372'; //top-middle
+var FEED_ONLINE_LEGAL = 'https://www.tadl.org/export/node/json/25242'; //middle-middle
+var FEED_ONLINE_EBOOKS = 'https://www.tadl.org/export/node/json/14040'; //???
 
 /* Feeds for Youth page */
-var FEED_YOUTH_DISPLAY = ''; //left
-var FEED_YOUTH_NEWBOOKS = ''; //left
-var FEED_YOUTH_REVIEWS = ''; //bottom-middle
-var FEED_YOUTH_EVENTS = ''; //right
-var FEED_YOUTH_RESOURCES = ''; //top-middle
-var FEED_YOUTH_AWARDWINS = ''; //middle-middle
+var FEED_YOUTH_DISPLAY = 'https://www.tadl.org/mobile/export/items/47/all/json'; //left
+var FEED_YOUTH_NEWBOOKS = 'https://www.tadl.org/mobile/export/items/52/json'; //left
+var FEED_YOUTH_REVIEWS = 'https://www.tadl.org/export/reviews/Youth/json'; //bottom-middle
+var FEED_YOUTH_EVENTS = 'https://www.tadl.org/mobile/export/events/formatted/json/27'; //right
+var FEED_YOUTH_RESOURCES = 'https://www.tadl.org/export/node/json/647'; //top-middle
+var FEED_YOUTH_AWARDWINS = 'https://www.tadl.org/export/node/json/644'; //middle-middle
 
 /* Feeds for Teens page */
 var FEED_TEENS_NEW = ''; //left
@@ -43,9 +43,68 @@ var FEED_TEENS_HOMEWORK = ''; //middle-middle
 var FEED_TEENS_LISTS = ''; //top-middle
 
 function showOnlinePage() {
+    cleanhouse();
+    cleandivs();
+    $('#working').show().spin('default');
+    $.getJSON(FEED_ONLINE_MEL, function(data) {
+        var template = Handlebars.compile($('#drupalnode-template').html());
+        var info = template(data);
+        $('#region-two').append(info);
+    });
+    $.getJSON(FEED_ONLINE_RESOURCES, function(data) {
+        var template = Handlebars.compile($('#drupalnode-template').html());
+        var info = template(data);
+        $('#region-two').prepend(info);
+        $('#working').hide().spin(false);
+    });
+    $.getJSON(FEED_ONLINE_LEGAL, function(data) {
+        var template = Handlebars.compile($('#drupalnode-template').html());
+        var info = template(data);
+        $('#region-one').append(info);
+    });
+    $.getJSON(FEED_ONLINE_EBOOKS, function(data) {
+        var template = Handlebars.compile($('#drupalnode-template').html());
+        var info = template(data);
+        $('#region-three').prepend(info);
+    });
 }
 function showYouthPage() {
+    cleanhouse();
+    cleandivs();
+    $('#working').show().spin('default');
+    $.getJSON(FEED_YOUTH_RESOURCES, function(data) {
+        var template = Handlebars.compile($('#drupalnode-template').html());
+        var info = template(data);
+        $('#region-one').append(info);
+    });
+    $.getJSON(FEED_YOUTH_AWARDWINS, function(data) {
+        var template = Handlebars.compile($('#drupalnode-template').html());
+        var info = template(data);
+        $('#region-one').append(info);
+    });
+    $.getJSON(FEED_YOUTH_DISPLAY, function(data) {
+        var template = Handlebars.compile($('#showfeatureditemboxall-template').html());
+        var info = template(data);
+        $('#region-one').prepend(info);
+    });
+    $.getJSON(FEED_YOUTH_NEWBOOKS, function(data) {
+        var template = Handlebars.compile($('#showfeatureditembox-template').html());
+        var info = template(data);
+        $('#region-one').prepend(info).prepend('<div class="card"><h4 class="title">Featured Items</h4></div>');
+    });
+    $.getJSON(FEED_YOUTH_EVENTS, function(data) {
+        var template = Handlebars.compile($('#showevents-template').html());
+        var info = template(data);
+        $('#region-three').html(info).prepend('<div class="card"><h4 class="title">Upcoming Events</h4></div>');
+    });
+    $.getJSON(FEED_YOUTH_REVIEWS, function(data) {
+        var template = Handlebars.compile($('#showreviews-template').html());
+        var info = template(data);
+        $('#region-two').append(info);
+        $('#working').hide().spin(false);
+    });
 }
+
 function showTeensPage() {
 }
 
