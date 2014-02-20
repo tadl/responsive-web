1443beb2861aa660e4772c6e6dbd554e2786c1a5
Scott Morey <smorey@swiftsure.tadl.org>
2014-02-19 19:13:37 -0500

    fixed bug with buttons not working after more details and display problem with line through title or author after more details

--- js/main-helper-functions.js
+++ js/main-helper-functions.js
@@ -10,7 +10,6 @@ var current_user = window.localStorage.getItem('current_user');
 
 
 $(document).ready(function() {
-    load_drupal_json('home');
     compile_templates(); 
     router.perform();
     
@@ -75,6 +74,7 @@ var drupal_json_url = "https://mel-catcher.herokuapp.com/drupal/drupal.json?cont
  $.getJSON(drupal_json_url, function(data) {
  		var payload = JSON.stringify(data)
         sessionStorage.setItem('home', payload );
+        load_drupal_json();
     });
 }
 else
